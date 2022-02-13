feca12054c2d8e48bd18da96a4abe50e5475f0af
shk3029 <shk30290@naver.com>
2022-02-13 14:23:33 +0900

    Initial commit

---
+++ src/test/java/com/example/studyspringcloudconfig/StudySpringCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyspringcloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringCloudConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
